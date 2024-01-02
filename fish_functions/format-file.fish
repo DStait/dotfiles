357f56f5172a3db12fd5fc6159028303de40d05a
Dominic Stait <dominic.stait@genomicsengland.co.uk>
2024-01-02 10:01:15 +0000

    updated format-file function

--- fish_functions/format-file.fish
+++ fish_functions/format-file.fish
@@ -1,26 +1,37 @@
 function format-file
-    set -l FILE "$argv"
+    set -l FILES $argv
 
-    if ! _check_ignored_format_file "$FILE"
-        return
+    for FILE in $FILES
+
+        # Don't bother with directories
+        if test -d "$FILE"
+            continue
+        end
+
+        # Check our ignored file types
+        if ! _check_ignored_format_file "$FILE"
+            return
+        end
+
+        set -l FILE_EXT (path extension "$FILE")
+        switch "$FILE_EXT"
+            case ".py"
+                _format_py_file "$FILE"
+            
+            case ".tf" ".tfvars"
+                _format_tf_file "$FILE"
+            
+            case ".json"
+                _format_json_file "$FILE"
+
+            case "*"
+                printf "%s%s%s not formatted. No formatter configured\n" (set_color red) "$FILE" (set_color normal)
+                continue
+        end
+
+        printf "%s%s%s formatted.\n" (set_color green) "$FILE" (set_color normal)
+
     end
-
-    set -l FILE_EXT (path extension "$FILE")
-    switch "$FILE_EXT"
-        case ".py"
-            _format_py_file "$FILE"
-        
-        case ".tf" ".tfvars"
-            _format_tf_file "$FILE"
-        
-        case ".json"
-            _format_json_file "$FILE"
-
-        case "*"
-            echo "No formatter configured for filetype: $FILE_EXT"
-    end
-
-    
 end
 
 function _check_ignored_format_file
@@ -29,7 +40,7 @@ function _check_ignored_format_file
         "$HOME/Library/Application Support/Code/User/settings.json" 
 
     if contains $FILE $IGNORED_FILES
-        echo "Not formatting $FILE as it is in ignored file list"
+        printf "%s%s%s not formatted as it is in ignored file list.\n" (set_color blue) "$FILE" (set_color normal)
         return 1
     end
     
