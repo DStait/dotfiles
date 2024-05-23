b93f67ee17f70fd699c5cd89cee44ffcdd341bca
Dominic Stait <dominic.stait@genomicsengland.co.uk>
2024-05-23 20:46:37 +0100

    change json format indent to 4

--- fish_functions/format-file.fish
+++ fish_functions/format-file.fish
@@ -61,6 +61,6 @@ function _format_json_file
     set -l FILE "$argv"
     set -l FILE_TEMP "$FILE.tmp"
 
-    jq --indent 2 . "$FILE" > "$FILE_TEMP"
+    jq --indent 4 . "$FILE" > "$FILE_TEMP"
     mv -f "$FILE_TEMP" "$FILE"
 end
